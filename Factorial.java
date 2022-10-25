6e08b840368edda703a40225f12fb1e9f6c4c026
Molla Hasim <72904271+MollaHasim@users.noreply.github.com>
2022-10-25 15:21:53 +0530

    Create Factorial.java

---
+++ Factorial.java
@@ -0,0 +1,16 @@
+// Find Factorial of a number using for loop
+//factorial of n (n!) = 1 * 2 * 3 * 4 * ... * n
+public class Factorial {
+
+    public static void main(String[] args) {
+
+        int num = 10;
+        long factorial = 1;
+        for(int i = 1; i <= num; ++i)
+        {
+            // factorial = factorial * i;
+            factorial *= i;
+        }
+        System.out.printf("Factorial of %d = %d", num, factorial);
+    }
+}
